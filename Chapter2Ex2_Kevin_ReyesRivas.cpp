dd19e8f54a138af364821c234fafe55f835de387
kar77626 <kar77626@HT-0232-19>
2024-08-30 10:28:30 -0400

    Add project files.

---
+++ Chapter2Ex2_Kevin_ReyesRivas.cpp
@@ -0,0 +1,29 @@
+/*
+Title: Chapter 2 Exercise 2 - Do something
+File Name: Chapter2Ex2_Kevin_ReyesRivas.cpp
+Programmer: Kevin Reyes Rivas
+Date: 08/2024
+Requirements: 
+The East Coast sales division of a company generates
+
+*/
+
+#include <iostream>
+using namespace std;
+
+ int main()
+{
+     int number1; 
+     int number2;
+     int answer; 
+     cout << "Please enter a number\n";
+     cin >> number1;
+     cout << "Please enter a second number\n";
+     cin >> number2;
+     answer = number1 + number2; 
+     cout << "The sum of " << number1 << " and " << number2 << " is " << answer;
+
+     return 0;//this is to return
+   
+}
+
